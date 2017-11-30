024218178853be0ea25b7e94e9411271839ad89b
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2017-11-30 14:01:12 -0700

    even more updates

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,6 +13,19 @@ class ArtistsController < ApplicationController
   	@artist = Artist.new
   end
 
+  def create
+  	@artist = Artist.new(artist_params)
+
+  	if @artist.save
+  		redirect_to artists_path @artist
+  	else
+  		render :new
+  	end
+  end
+
   private 
   #set artist params
+  def artist_params
+  	params.require(:artist).permit()
+  end
 end
